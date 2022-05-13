a6d3fe2618c6d0e6bc63648ce32eec47100ac531
christian-ferre <christian.ferre@e-campus.uab.cat>
2022-05-13 21:02:02 +0200

    emotion classificator

--- EmotionClassificator/EmotionClassificator.py
+++ EmotionClassificator/EmotionClassificator.py
@@ -1,15 +1,21 @@
 from fer import FER
 import matplotlib.pyplot as plt
-
+import cv2
 
 class EmotionClassificator:
     def __init__(self):
-        self.emo_detector = FER(mtcnn=True)
         self.emociones = {"angry": "enfadado", "disgust": "disgustado", "fear": "asustado", "happy": "feliz",
                           "sad": "triste", "surprise": "sorprendido", "neutral": "neutral"}
 
     def classificar(self, img):
-        captured_emotions = self.emo_detector.detect_emotions(img)
-        dominant_emotion, emotion_score = self.emo_detector.top_emotion(img)
-        frase = "Escaneando resultados. Te ves " + self.emociones[dominant_emotion]
+        emo_detector = FER(mtcnn=True)
+        captured_emotions = emo_detector.detect_emotions(img)
+        dominant_emotion, emotion_score = emo_detector.top_emotion(img)
+        frase = "Escaneando resultados. Te ves " + str(self.emociones[dominant_emotion])
         return frase
+
+
+img = cv2.imread("juan.png")
+a = EmotionClassificator()
+print(a.classificar(img))
+
