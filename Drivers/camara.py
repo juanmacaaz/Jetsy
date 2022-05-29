da176e2ad27b6c2b301f0f04706fb20ff015ca2c
Miguel del Arco <migueldemollet10@gmail.com>
2022-05-29 22:59:17 +0200

    Update drivers

--- Drivers/camara.py
+++ Drivers/camara.py
@@ -12,27 +12,26 @@ def gstreamer_pipeline(sensor_id=0, capture_width=1920, capture_height=1080, dis
     	)
 
 def show_camera():
-    	window_title = "Camera"
-
-    	# To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+	window_title = "Camera"
+	# To flip the image, modify the flip_method parameter (0 and 2 are the most common)
 	print(gstreamer_pipeline(flip_method=0))
-    	video_capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+	video_capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 
 	if video_capture.isOpened():
-        	try:
-            		window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
-            		while True:
-                		ret_val, frame = video_capture.read()
-                		if cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
-                    			cv2.imshow(window_title, frame)
-                		else:
-                    			break
-                		keyCode = cv2.waitKey(10) & 0xFF
-                		if keyCode == 27 or keyCode == ord('q'):
-                    			break
-        	finally:
-            		video_capture.release()
-            		cv2.destroyAllWindows()
+		try:
+			window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
+			while True:
+				ret_val, frame = video_capture.read()
+				if cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
+					cv2.imshow(window_title, frame)
+				else:
+					break
+				keyCode = cv2.waitKey(10) & 0xFF
+				if keyCode == 27 or keyCode == ord('q'):
+					break
+		finally:
+			video_capture.release()
+			cv2.destroyAllWindows()
 	else:
 		print("Error: Unable to open camera")
 
--- Drivers/frontal_proximity_sensor.py
+++ Drivers/frontal_proximity_sensor.py
@@ -1,15 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
-sensor = 13
+sensor = 16
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(sensor,GPIO.IN)
 detect_object = True
- 
+
 while True:
     detect_object = GPIO.input(sensor)
     if not (detect_object):
-        print("Object Detected")
+        print("1")
         detect_object = True
-        time.sleep(0.5)
+    else:
+        print("0")
+
+    time.sleep(0.5)
